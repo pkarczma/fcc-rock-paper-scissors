74d632e97f2e4c433639230794376051ab225df5
Przemyslaw Karczmarczyk <przemyslaw.karczmarczyk@cern.ch>
2020-12-17 18:47:52 +0000

    feat: add random choice

--- RPS.py
+++ RPS.py
@@ -1,5 +1,7 @@
 # The example function below keeps track of the opponent's history and plays whatever the opponent played two plays ago. It is not a very good player so you will need to change the code to pass the challenge.
 
+import random
+
 def player(prev_play, opponent_history=[]):
     opponent_history.append(prev_play)
 
@@ -7,4 +9,4 @@ def player(prev_play, opponent_history=[]):
     if len(opponent_history) > 2:
         guess = opponent_history[-2]
 
-    return guess
+    return random.choice([guess, "R", "P", "S"])
