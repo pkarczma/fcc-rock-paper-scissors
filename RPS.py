fe21c4b9b3e17ce71702c0d57c73106981e1acb4
Przemyslaw Karczmarczyk <przemyslaw.karczmarczyk@cern.ch>
2020-12-30 11:20:24 +0000

    feat: build DataFrame using opponent history

--- RPS.py
+++ RPS.py
@@ -1,12 +1,23 @@
 # The example function below keeps track of the opponent's history and plays whatever the opponent played two plays ago. It is not a very good player so you will need to change the code to pass the challenge.
 
 import random
+import pandas as pd
+
+df = None
 
 def player(prev_play, opponent_history=[]):
-    opponent_history.append(prev_play)
+    global df
+    if prev_play == '':
+        df = pd.DataFrame()
+        opponent_history = []
+    else:
+        opponent_history.append(prev_play)
 
     guess = "R"
     if len(opponent_history) > 2:
         guess = opponent_history[-2]
+
+    if len(opponent_history) >= 5:
+        df = df.append(pd.Series(opponent_history[-5:]), ignore_index=True)
 
     return random.choice([guess, "R", "P", "S"])
