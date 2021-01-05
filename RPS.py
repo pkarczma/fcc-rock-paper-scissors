f97f3014525748bdbdf9068ac2aaf4b1a458ccd2
Przemyslaw Karczmarczyk <przemyslaw.karczmarczyk@cern.ch>
2021-01-05 15:05:48 +0000

    chore: renaming variables

--- RPS.py
+++ RPS.py
@@ -3,11 +3,10 @@
 import random
 import numpy as np
 import pandas as pd
-import tensorflow as tf
 from tensorflow import keras
 
-df_x = None
-df_y = None
+df_train_x = None
+df_train_y = None
 model = None
 hlen = 5
 hentries = 20
@@ -15,10 +14,10 @@ moves = ['R', 'P', 'S']
 ideal_response = {'R': 'P', 'P': 'S', 'S': 'R'}
 
 def player(prev_play, opponent_history=[]):
-    global df_x, df_y, model
+    global df_train_x, df_train_y, model
     if prev_play == '':
-        df_x = pd.DataFrame()
-        df_y = pd.DataFrame()
+        df_train_x = pd.DataFrame()
+        df_train_y = pd.DataFrame()
         model = keras.Sequential([
             keras.layers.Dense(hlen, input_shape=(hlen,)),
             keras.layers.Dense(3, activation='softmax')
@@ -33,13 +32,13 @@ def player(prev_play, opponent_history=[]):
     guess = random.choice(moves)
 
     if len(opponent_history) > hlen:
-        df_x = df_x.append(pd.Series(opponent_history[-(hlen+1):-1]), ignore_index=True).astype('int8')
-        df_y = df_y.append(pd.Series(opponent_history[-1]), ignore_index=True).astype('int8')
+        df_train_x = df_train_x.append(pd.Series(opponent_history[-(hlen+1):-1]), ignore_index=True).astype('int8')
+        df_train_y = df_train_y.append(pd.Series(opponent_history[-1]), ignore_index=True).astype('int8')
 
     if len(opponent_history) >= (hlen+hentries):
-        model.fit(df_x, df_y, epochs=3, verbose=0)
-        test = pd.DataFrame([opponent_history[-hlen:]])
-        predictions = model.predict([test])
+        model.fit(df_train_x, df_train_y, epochs=3, verbose=0)
+        df_test_x = pd.DataFrame([opponent_history[-hlen:]])
+        predictions = model.predict([df_test_x])
         guess = ideal_response[moves[np.argmax(predictions[0])]]
 
     return guess
