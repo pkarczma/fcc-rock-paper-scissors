71ba74e9abc89f31abb9b422a69bfc29fd41e2d9
Przemyslaw Karczmarczyk <przemyslaw.karczmarczyk@cern.ch>
2021-02-05 11:23:36 +0000

    chore: add code documentation

--- RPS.py
+++ RPS.py
@@ -7,7 +7,6 @@ moves = ['R', 'P', 'S']
 ideal_response = {'R': 'P', 'P': 'S', 'S': 'R'}
 
 # Variables for Keras method
-use_keras = False
 df_train_x = None
 df_train_y = None
 model = None
@@ -27,8 +26,12 @@ def player(prev_play, opponent_history=[]):
     # Use a random choice by default
     guess = random.choice(moves)
 
+    # Use Markov Chain method
+    # - wins with all players with > 60% efficiency
+    # - possible to adjust results with 'memory' variable
     if use_markov_chain == True:
         global matrix, my_history, opp_history
+        # initialize variables in the first game
         if prev_play == '':
             for pair_key in pair_keys:
                 matrix[pair_key] = {'R': 1 / 3,
@@ -36,23 +39,39 @@ def player(prev_play, opponent_history=[]):
                                     'S': 1 / 3}
             opp_history = []
             my_history = []
+        # otherwise, add previous opponent play to the history
         else:
             opp_history.append(prev_play)
 
+        # make a prediction when enough entries in the history
         if len(my_history) >= 2:
+            # create a pair from 2 plays ago
             pair_key = my_history[-2] + opp_history[-2]
+            # introduce a memory loss of earlier observations for that pair,
+            # memory decay speed can be adjusted using 'memory' variable
             for key in matrix[pair_key]:
                 matrix[pair_key][key] = memory * matrix[pair_key][key]
+            # then, update matrix for that pair
             matrix[pair_key][prev_play] += 1
+
+            # create a pair from the previous play
             curr_key = my_history[-1] + opp_history[-1]
+            # if the matrix values are not equal for that pair,
+            # make a prediction using the move with the higest value
             if max(matrix[curr_key].values()) != min(matrix[curr_key].values()):
                 prediction = max([(v, k) for k, v in matrix[curr_key].items()])[1]
                 guess = ideal_response[prediction]
 
+        # append my guess to the history
         my_history.append(guess)
-        
-    if use_keras == True:
+
+    # [Deprecated] Use Keras library instead
+    # Warning:
+    # - 1st attempt for this problem that does not win with all players
+    # - requires much more computation time
+    if use_markov_chain == False:
         global df_train_x, df_train_y, model
+        # initialize variables in the first game
         if prev_play == '':
             df_train_x = pd.DataFrame()
             df_train_y = pd.DataFrame()
@@ -64,13 +83,18 @@ def player(prev_play, opponent_history=[]):
                 loss='sparse_categorical_crossentropy',
                 metrics=['accuracy'])
             opponent_history = []
+        # otherwise, add previous opponent play to the history
         else:
             opponent_history.append(moves.index(prev_play))
 
+        # use opponents play history to build a dataframe of opponent history
+        # series of length of 'hlen' each as x axis and their following moves
+        # as y axis for the training after at least 'hlen+1' plays
         if len(opponent_history) > hlen:
             df_train_x = df_train_x.append(pd.Series(opponent_history[-(hlen+1):-1]), ignore_index=True).astype('int8')
             df_train_y = df_train_y.append(pd.Series(opponent_history[-1]), ignore_index=True).astype('int8')
 
+        # after 'hlen+hentries' plays, fit the model and make a prediction
         if len(opponent_history) >= (hlen+hentries):
             model.fit(df_train_x, df_train_y, epochs=5, verbose=0)
             df_test_x = pd.DataFrame([opponent_history[-hlen:]])
